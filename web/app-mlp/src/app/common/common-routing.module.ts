e3ac42e0cbe4d02f0d2ce03c6050cf9a7606b7e4
Neha Shamreen <nhshamreen@gmail.com>
2018-06-28 19:34:06 -0400

    signup page and welcome page layout completed

--- web/app-mlp/src/app/common/common-routing.module.ts
+++ web/app-mlp/src/app/common/common-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
 
 const routes: Routes = [
-  {path:"login",component:LoginComponent}
+  {path:"login",component:LoginComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"welcome", component:WelcomeComponent},
+  {path:"toolbar",component:ToolbarComponent}
 ];
 
 @NgModule({
--- web/app-mlp/src/app/common/common.module.ts
+++ web/app-mlp/src/app/common/common.module.ts
@@ -3,18 +3,22 @@ import { CommonModule as NgCommonModule} from '@angular/common';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { CommonRoutingModule } from './common-routing.module';
 import { LoginComponent } from './login/login.component';
-import{MatFormFieldModule,MatInputModule,MatButtonModule} from '@angular/material';
+import{MatFormFieldModule,MatInputModule,MatButtonModule, MatToolbarModule, MatIconModule} from '@angular/material';
 import{FormsModule} from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 @NgModule({
   imports: [
     NgCommonModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule,MatButtonModule,
+    MatInputModule,MatButtonModule,MatToolbarModule,MatIconModule,
     FlexLayoutModule,
     CommonRoutingModule
   ],
-  declarations: [LoginComponent], 
-  exports: [FormsModule,MatFormFieldModule,MatInputModule,MatButtonModule,FlexLayoutModule]
+  declarations: [LoginComponent, SignupComponent, WelcomeComponent, ToolbarComponent], 
+  exports: [FormsModule,MatFormFieldModule,MatInputModule,MatButtonModule,MatToolbarModule,MatIconModule,
+    FlexLayoutModule]
 })
 export class CommonModule {  }
